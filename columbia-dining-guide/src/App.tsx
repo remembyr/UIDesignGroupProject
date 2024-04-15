b050edf8b8a9cff6ffa07742efde2df2a049fde2
Embyr W <embyrwilliams@gmail.com>
2024-04-15 14:13:58 -0400

    Add routes to App

--- columbia-dining-guide/src/App.tsx
+++ columbia-dining-guide/src/App.tsx
@@ -1,8 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 import Home from "./components/Home";
+import MacroMenu from "./components/MacroMenu";
 
 function App() {
   return (
-    <Home />
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/learn/macros" element={<MacroMenu />}/>
+                    {/*Add other paths here */}
+                </Routes>
   );
 }
 
--- columbia-dining-guide/src/components/MacroMenu.tsx
+++ columbia-dining-guide/src/components/MacroMenu.tsx
@@ -1,3 +1,22 @@
 //slide 4, 9, 14
+import React from "react";
+import { MyModal } from "./Modal";
+import { SimpleNavbar } from "./SimpleNavbar";
+import { Card } from "flowbite-react";
 
-export {}
+
+//slide 2
+
+function MacroMenu() {
+  return (
+    <>
+      <SimpleNavbar />
+      <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+        <h1 className="text-2xl dark:text-white">Macro Menu</h1>
+        <MyModal />
+      </main>
+    </>
+  );
+}
+
+export default MacroMenu;
