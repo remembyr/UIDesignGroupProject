0acb4937e8ca3842a19f9d3641afa27f9fef2c00
Eki <ekiuzams@gmail.com>
2024-04-15 12:42:45 -0400

    Adding Navbar

--- columbia-dining-guide/src/components/Home.tsx
+++ columbia-dining-guide/src/components/Home.tsx
@@ -1,16 +1,18 @@
 import { DarkThemeToggle } from "flowbite-react";
 import { MyModal } from "./Modal";
+import { SimpleNavbar } from "./SimpleNavbar";
 
 //slide 2
 
 function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      <h1 className="text-2xl dark:text-white">
-        Flowbite React + Create React App
-      </h1>
-      <MyModal />
-    </main>
+    <>
+      <SimpleNavbar />
+      <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+        <h1 className="text-2xl dark:text-white">Dining Guide</h1>
+        <MyModal />
+      </main>
+    </>
   );
 }
 
---
+++ columbia-dining-guide/src/components/SimpleNavbar.tsx
@@ -0,0 +1,38 @@
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+
+export function SimpleNavbar() {
+  return (
+    <Navbar fluid rounded>
+      <NavbarBrand href="https://flowbite-react.com">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d8/Fruit.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Agnes Scott College Seal"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Flowbite React
+        </span>
+      </NavbarBrand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink href="#">About</NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
+        <NavbarLink href="#">Pricing</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
