6c30dbfa511f8e0ae340e7312fb7c91a85f013cb
benjaminpstein <bpstein21@gmail.com>
2024-04-15 22:48:16 -0400

    Styled homepage

--- columbia-dining-guide/src/components/Home.tsx
+++ columbia-dining-guide/src/components/Home.tsx
@@ -1,14 +1,24 @@
-import { DarkThemeToggle } from "flowbite-react";
-import { MyModal } from "./Modal";
+import {Button} from "flowbite-react";
+import {useNavigate} from "react-router-dom";
 
 //slide 2
 
 function Home() {
+
+  const navigate = useNavigate()
+
   return (
     <>
-      <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-        <h1 className="text-2xl dark:text-white">Dining Guide</h1>
-        <MyModal />
+      <main className="min-h-screen items-center gap-2 dark:bg-gray-800">
+        <div className="grid-cols-6 gap-4 items-center ml-20">
+          <h1 className="text-7xl mb-8 mt-40">
+            Dining Guide
+          </h1>
+          <h3 className="text-3xl text-gray-600 font-normal mb-8">
+            Learn how to balance Columbia Dining meals.
+          </h3>
+          <Button color={"blue"} onClick={() => navigate('/learn')} className={"font-normal"} outline>Start Learning</Button>
+        </div>
       </main>
     </>
   );
