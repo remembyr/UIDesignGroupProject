ec673111f54b923535c67b5a3f8c6723c1d62352
Embyr W <embyrwilliams@gmail.com>
2024-05-03 15:41:15 -0400

    Add UI for quiz summary page

--- columbia-dining-guide/src/components/QuizResults.tsx
+++ columbia-dining-guide/src/components/QuizResults.tsx
@@ -1,9 +1,6 @@
-/* eslint-disable tailwindcss/no-custom-classname */
 import { useModal } from "../contexts/ModalContext";
-import { Card, Button, Modal } from "flowbite-react";
+import { Card, Button, Modal, Spinner } from "flowbite-react";
 import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import MacroPlate from "./Plate/MacroPlate";
 import {useNavigate} from "react-router-dom";
 
 interface Food {
@@ -15,86 +12,145 @@ export default function QuizResults() {
 
     const navigate = useNavigate();
     const { isModalOpen, setModalOpen } = useModal();
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const [foods, setFoods] = useState<Food[]>([]);
 
-    const [userChoices50Section, setUserChoices50Section] = useState<Food[]>([]);
-    const [userChoices25TopSection, setUserChoices25TopSection] = useState<Food[]>([]);
-    const [userChoices25BottomSection, setUserChoices25BottomSection] = useState<Food[]>([]);
-
-    /* dropping onto plate */
-
-    const handleDrop50 = (event: React.DragEvent<HTMLDivElement>) => {
-        // no-op
-    };
-
-    const handleDrop25Top = (event: React.DragEvent<HTMLDivElement>) => {
-        // no-op
-        
-    };
-
-    const handleDrop25Bottom = (event: React.DragEvent<HTMLDivElement>) => {
-        // no-op  
-    };
-
-    const removeFromPlate50 = (food: string) => {
-        // no-op
-    };
-
-    const removeFromPlate25Top = (food: string) => {
-        // no-op
-    };
-
-    const removeFromPlate25Bottom = (food: string) => {
-        // no-op
-    };
-
-    const reenterQuiz = () => {
-        navigate("../quiz")
-    }
-
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-
-                    <div className="my-9 mb-[10rem]">
-                        <MacroPlate
-                            onDrop50={handleDrop50}
-                            onDrop25Top={handleDrop25Top}
-                            onDrop25Bottom={handleDrop25Bottom}
-
-                            macro="Carb"
-
-                            food50={userChoices50Section}
-                            food25_1={userChoices25TopSection}
-                            food25_2={userChoices25BottomSection}
-                            
-                            removeFromPlate50={removeFromPlate50}
-                            removeFromPlate25Top={removeFromPlate25Top}
-                            removeFromPlate25Bottom={removeFromPlate25Bottom}
-                        />
-
+        <>
+        <main className="flex min-h-screen items-center justify-center dark:bg-gray-800">
+          <Card className="max-w-5xl">
+            <h1 className="text-2xl">Well done!</h1>
+            <h2 className="text-xl">
+              You passed all three challenges. Here are your final dish-tributions:
+            </h2>
+  
+            {isLoading ? (
+              <div className="mb-2 flex items-center justify-between px-24">
+                <Spinner />
+                <Spinner />
+                <Spinner />{" "}
+              </div>
+            ) : (
+              <div className="mb-2 gap-4 flex items-center justify-between">
+              <Card
+                    className="max-w-md"
+                  >
+                    <div className="justify-center" style={{ display: "flex" }}>
+                      <div className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Buffalo Chicken Wrap
+                      </div>
+                        <span style={{ paddingLeft: "3%", paddingTop: "2%" }}>
+                          <svg
+                            className="size-3.5 text-green-600 dark:text-blue-300 lg:size-4"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 16 12"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M1 5.917 5.724 10.5 15 1.5"
+                            />
+                          </svg>
+                        </span>
                     </div>
-                   
-
-                </div>
-                <div className="col-md-6">
-                    <div className="my-9 bg-white shadow-md p-6 rounded-xl">
-                        <p className="text-[1.5rem] font-semibold">Well done!</p>
-
-                        <p className="text-[1.25rem]">Your final dish-tribution was:</p>
-
-                        <p id="carbs-results">Carbs: </p>
-                        <p id="protein-results">Protein: </p>
-                        <p id="fat-results">Fat: </p>
-
-                        <Button color={"blue"} onClick={reenterQuiz} className="font-normal my-[1.875rem]" outline>Try Again</Button>
+                    <div className="text-xl text-gray-400 font-medium">
+                        You selected:<br></br><span className="text-gray-900 pl-4">Food 1</span><br /><span className="text-gray-900 pl-4">Food 2</span>
                     </div>
-                    
-                </div>
-
-            </div>
-        </div>
+                    <div className="text-xl text-gray-400 font-medium">
+                        Macronutrient breakdown of this meal:<br></br>
+                        <span className="text-gray-900 pl-4">Protein: 0g (0%)</span><br />
+                        <span className="text-gray-900 pl-4">Carbohydrates: 0g (0%)</span><br />
+                        <span className="text-gray-900 pl-4">Fat: 0g (0%)</span>
+                    </div>
+                  </Card>
+                  <Card
+                    className="max-w-md"
+                  >
+                    <div className="justify-center" style={{ display: "flex" }}>
+                      <div className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Lentil Curry
+                      </div>
+                        <span style={{ paddingLeft: "3%", paddingTop: "2%" }}>
+                          <svg
+                            className="size-3.5 text-green-600 dark:text-blue-300 lg:size-4"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 16 12"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M1 5.917 5.724 10.5 15 1.5"
+                            />
+                          </svg>
+                        </span>
+                    </div>
+                    <div className="text-xl text-gray-400 font-medium">
+                        You selected:<br></br><span className="text-gray-900 pl-4">Food 1</span><br /><span className="text-gray-900 pl-4">Food 2</span>
+                    </div>
+                    <div className="text-xl text-gray-400 font-medium">
+                        Macronutrient breakdown of this meal:<br></br>
+                        <span className="text-gray-900 pl-4">Protein: 0g (0%)</span><br />
+                        <span className="text-gray-900 pl-4">Carbohydrates: 0g (0%)</span><br />
+                        <span className="text-gray-900 pl-4">Fat: 0g (0%)</span>
+                    </div>
+                  </Card>
+                  <Card
+                    className="max-w-md"
+                  >
+                    <div className="justify-center" style={{ display: "flex" }}>
+                      <div className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Caesar Salad
+                      </div>
+                        <span style={{ paddingLeft: "3%", paddingTop: "2%" }}>
+                          <svg
+                            className="size-3.5 text-green-600 dark:text-blue-300 lg:size-4"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 16 12"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M1 5.917 5.724 10.5 15 1.5"
+                            />
+                          </svg>
+                        </span>
+                    </div>
+                    <div className="text-xl text-gray-400 font-medium">
+                        You selected:<br></br><span className="text-gray-900 pl-4">Food 1</span><br /><span className="text-gray-900 pl-4">Food 2</span>
+                    </div>
+                    <div className="text-xl text-gray-400 font-medium">
+                        Macronutrient breakdown of this meal:<br></br>
+                        <span className="text-gray-900 pl-4">Protein: 0g (0%)</span><br />
+                        <span className="text-gray-900 pl-4">Carbohydrates: 0g (0%)</span><br />
+                        <span className="text-gray-900 pl-4">Fat: 0g (0%)</span>
+                    </div>
+                  </Card>
+            
+              </div>
+            )}
+            <p className="font-normal text-gray-700">
+              Great work passing the quiz. If you want to take it again, hit the button below.
+            </p>
+              <Button
+                onClick={() => {setModalOpen(false); navigate("../quiz");}}
+                style={{ backgroundColor: "#008901" }}
+              >
+                Try Again
+              </Button>
+          </Card>
+        </main>
+      </>
     )
 }
